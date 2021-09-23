ad51af3b7039ad74e4d5c5d49d84db095ba1d81c
Emily Dorgan <wmdorgan@gmail.com>
2021-09-22 20:43:19 -0700

    initialized a nested array that has the 4 types of characters (uppercase, lowercase, numbers and special chars) that the password can be

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //nested array containing the allowable characters in the password. Subarrays are: 0: Uppercase letters, 1: lowercase letters, 2: numbers, 3: special characters
   var charSet = [
-    ["A", "B", "C", "D"]
-  ]
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+  ];
+  
 }
 
 // Add event listener to generate button
@@ -19,7 +24,7 @@ generateBtn.addEventListener("click", writePassword);
 console.log(generateBtn);
 
 //Pseudocode:
-// 1. make a list of number, a list of upper case letters, a list of lower case letters and a list of special characters (array seems best)
+// 1. make a list of numbers, a list of upper case letters, a list of lower case letters and a list of special characters (array seems best)
 // 2. When the user has clicked on the 'generate password button',
 // 3. Ask the user if they want uppercase letters in their password, if yes then keep track of that
 // 4. Ask the user if they want lowercase letters in their password, if yes then keep track of that
