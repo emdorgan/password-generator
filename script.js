8e43c54a3333936b190563049983e9528c38daae
Emily Dorgan <wmdorgan@gmail.com>
2021-09-22 21:09:10 -0700

    moved the charSet array to have a global scope and defined an undefined function

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var charSet = [
+  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  function generatePassword(){
   //nested array containing the allowable characters in the password. Subarrays are: 0: Uppercase letters, 1: lowercase letters, 2: numbers, 3: special characters
-  var charSet = [
-    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
-  ];
   
+  var useUpper = confirm("Would you like to use uppercase characters?");
+  };
+
+
 }
 
 // Add event listener to generate button
